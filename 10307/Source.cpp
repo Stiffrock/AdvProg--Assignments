6588e398e77386e2a7e0697d6b07a3c683641036
Stiffrock <christofer.malmberg@gmail.com>
2016-09-24 22:02:47 +0200

    Some pretty good progress..

--- 10307/Source.cpp
+++ 10307/Source.cpp
@@ -2,6 +2,7 @@
 #include <string>
 #include <vector>
 #include <queue>
+#include <algorithm>
 
 using namespace std;
 
@@ -21,11 +22,16 @@ struct edge
 {
 	point u, v;
 	int d;
+
+	bool operator < (const edge& e1) const
+	{
+		return d < e1.d;
+	}
 };
 
 vector<edge> edges;
 
-void bfs(point startp, point endp)
+void bfs(point startp, vector<point> endp) // point endp)
 {
 	for (int i = 0; i < 50; i++)
 		for (int j = 0; j < 50; j++)
@@ -34,7 +40,6 @@ void bfs(point startp, point endp)
 			dist[i][j] = INT_MAX;
 		}
 
-	
 	queue<point> q;
 	q.push(startp);
 	visited[startp.x][startp.y] = true;
@@ -48,6 +53,10 @@ void bfs(point startp, point endp)
 		edge e;
 		q.pop();
 
+	/*	if (u.x == endp.x && u.y == endp.y)
+		{
+			return;
+		}*/
 		
 		if (u.x >= 0 && u.x < n && A[u.x + 1][u.y] != '#'&& A[u.x + 1][u.y] != '\0' && visited[u.x +1][u.y] == false)
 		{
@@ -83,17 +92,79 @@ void bfs(point startp, point endp)
 			e.u = u; e.v = v;
 			edges.push_back(e);
 			q.push(v);
-		}
-		
+		}		
 	}
-	
+}
 
 
+struct DisjointSets
+{
+	int *parent, *rank;
+	int n;
+
+	DisjointSets(int n)
+	{
+		this->n = n;
+		parent = new int[n + 1];
+		rank = new int[n + 1];
+
+		for (int i = 0; i < n; i++) //samma som untion find init
+		{
+			rank[i] = 0;
+			parent[i] = i;
+		}
+	}
+
+	int find(int u)
+	{
+		if (u != parent[u])
+			parent[u] = find(parent[u]);
+		return parent[u];	
+	}
+
+	void merge(int x, int y)
+	{
+		x = find(x), y = find(y);
+
+		if (rank[x] > rank[y])
+			parent[y] = x;
+		else
+			parent[x] = y;
+		if (rank[x] == rank[y])
+			rank[y]++;
+	}
+};
+
+
+int Kruskal()
+{
+	int mst_weight = 0;
+	sort(edges.begin(), edges.end());
+	vector<edge> MST;
+
+	DisjointSets ds(edges.size());
+
+	for (int i = 0; i < edges.size(); i++)
+	{
+		int u = edges[i].u.x;
+		int v = edges[i].u.y;
+		int set_u = ds.find(u);
+		int set_v = ds.find(v);
+
+
+		//Check if edge is creating a cycle or not.
+		//Cycle is created if u and v belong to same set.
+		if (set_u != set_v)
+		{
+			mst_weight += i;
+		}
+		ds.merge(set_u, set_v);
+	}
+	return mst_weight;
 }
 
 int main()
 {
-
 	cin >> c;
 	while (c--)
 	{
@@ -118,9 +189,13 @@ int main()
 				}
 			}
 		}
-		bfs(startp, endp[0]);
+		/*for (int i = 0; i < endp.size(); i++)
+			bfs(startp, endp[i]);*/
+
+		bfs(startp, endp); // Du ska använda endp och göra en union find ner till startp, för alla aliens. Efter sortering..
 		edges;
-		dist;
+		int ans = Kruskal();
+		cout << ans;
 	}
 	cin.get();
 	return 0;
